0a7043f432edd1e43a1478f1b88e520b05b44ec2
jertlemiah <jeremiah.plauche@gmail.com>
2020-12-01 23:32:41 -0600

    Got some JSON functionality working for populating the Fate Reasons

---
+++ Assets/Scripts/MenuFateReason.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Newtonsoft.Json;
+
+public class MenuFateReason : MonoBehaviour
+{
+    public string fileName;
+    public List<FateReason> fateReasons = new List<FateReason>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //JObject obj = JObject.Parse(File.ReadAllText(fileName));
+        fateReasons = JsonConvert.DeserializeObject<List<FateReason>>(File.ReadAllText(fileName));
+        Debug.Log(fateReasons.Count + " fateReasons found in json file");
+        foreach (FateReason reason in fateReasons)
+        {
+            
+            Debug.Log(reason.name + " has details?: " + reason.hasDetails);
+            if (reason.hasDetails == true)
+            {
+                foreach (string detail in reason.details)
+                {
+                    Debug.Log(" option: " + detail);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[System.Serializable]
+public class FateReason
+{
+    public string name,
+        sentence;
+    public bool hasDetails = false,
+        hasKiller = false;
+    public string[] details;
+}
